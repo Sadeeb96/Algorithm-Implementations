a9c77279e664150e083caa9d0f1d7ef2dd096c29
Ahsan Sadeeb <sadeeb.mufc@gmail.com>
2019-07-15 00:00:00 +0600

    Add files via upload

---
+++ karatsubaMultiplication.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+ll stringToInt(string s)
+{
+    ll p=1;
+    ll sum=0;
+    for(int i=s.size()-1;i>=0;i--){
+        sum+=(p*(s[i]-'0'));
+        p*=10;
+    }
+    return sum;
+}
+ll product(string x,string y)
+{
+    if(x.size()==1 || y.size()==1){
+        return stringToInt(x)*stringToInt(y);
+    }else{
+        string a=x.substr(0,x.size()/2);
+        string b=x.substr(x.size()/2,x.size()-1);
+        string c=y.substr(0,y.size()/2);
+        string d=y.substr(y.size()/2,y.size()-1);
+        //cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+        return pow(10,y.size())*product(a,c)+product(b,d)+pow(10,y.size()/2)*(product(a,d)+product(b,c));
+    }
+}
+int main()
+{
+    cout<<product("123456","123456");
+}
